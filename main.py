5b3e1ac003edaa2b84e593bda73bf5293ad4b8ab
Ywast1k <kbynevskij@yandex.ru>
2024-01-18 19:45:07 +0100

    finish

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI': lst})
 
-table = pd.get_dummies(data, columns=['whoAmI'])
-
-print(table.head())
+data['tmp'] = 1
+data.set_index([data.index, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value = 0).astype(int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+print(data)
